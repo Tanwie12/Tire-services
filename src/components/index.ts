beee017fff678fd51b903fe9996e3a60e2ac2912
Tanwie12 <65525163+Tanwie12@users.noreply.github.com>
2024-11-20 22:09:59 +0100

    d

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,15 +5,14 @@ import Link from './buttons/Link';
 import NavButton from './buttons/NavButton';
 
 
-import SocialLink from './socials/SocialLink';
-import AuthorImage from './ui/AuthorImage';
+
 import Cursor from './ui/Cursor';
 
 import Sidebar from './ui/Sidebar';
 
 
 export {
-  AuthorImage,
+ 
   Button,
   Cursor,
   DarkModeButton,
@@ -23,6 +22,6 @@ export {
  
   Sidebar,
  
-  SocialLink,
+
  
 };
